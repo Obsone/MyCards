4a6a4e0d94ce3b48d3dfa78ff28cb52788f6085a
Obsone <jsdcone@mgail.com>
2022-05-19 22:51:25 +0800

    small changes

--- src/Card.js
+++ src/Card.js
@@ -8,16 +8,22 @@ const Card = () => {
         <div className="app">
             <div className="info">
                 <div className="card" onClick={() => setIsToggled(!isToggled)}>
-                    <div class={`front ${isToggled ? "flipped" : ""}`}>front</div>
-                    <div class={`back ${isToggled ? "flipped" : ""}`}>back</div>
+                    <div class={`front ${isToggled ? "flipped" : ""}`}>front Info
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class={`back ${isToggled ? "flipped" : ""}`}>back Info</div>
                 </div>
                 <div className="card" onClick={() => setIsToggled(!isToggled)}>
-                    <div class={`front ${isToggled ? "flipped" : ""}`}>front</div>
-                    <div class={`back ${isToggled ? "flipped" : ""}`}>back</div>
+                    <div class={`front ${isToggled ? "flipped" : ""}`}>front Info
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class={`back ${isToggled ? "flipped" : ""}`}>back Info</div>
                 </div>
                 <div className="card" onClick={() => setIsToggled(!isToggled)}>
-                    <div class={`front ${isToggled ? "flipped" : ""}`}>front</div>
-                    <div class={`back ${isToggled ? "flipped" : ""}`}>back</div>
+                    <div class={`front ${isToggled ? "flipped" : ""}`}>front Info
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class={`back ${isToggled ? "flipped" : ""}`}>back Info</div>
                 </div>
             </div>
         </div>
